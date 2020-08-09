ac0ba7b7b098aaeb49b42e504bb50ba01fa36ebd
unknown <japerez.ph@gmail.com>
2020-08-10 06:31:50 +0800

    displaying an image from assets folder

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -12,46 +12,65 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 500, //Parent sets the height of list view
-      child: ListView.builder(
-        itemBuilder: (ctx, index) {
-          return Card(
-            child: ListTile(
-              title: Text(
-                transactions[index].title,
-                overflow: TextOverflow.ellipsis,
-                // ignore: deprecated_member_use
-                style: Theme.of(context).textTheme.title,
-                // style: TextStyle(
-                //   color: Colors.black87,
-                // ),
-              ),
-              subtitle: Text(
-                DateFormat('MMM dd, yyyy').format(transactions[index].date),
-                style: TextStyle(
-                  fontSize: 12,
-                  color: Colors.grey,
+      child: transactions.isEmpty
+          ? Column(
+              children: <Widget>[
+                Text(
+                  'No transactions added yet',
+                  // ignore: deprecated_member_use
+                  style: Theme.of(context).textTheme.title,
                 ),
-              ),
-              leading: Container(
-                padding: EdgeInsets.symmetric(
-                  vertical: 10,
+                SizedBox(
+                  height: 20,
                 ),
-                width: 120,
-                child: Text(
-                  '\$${transactions[index].amount.toStringAsFixed(2)}',
-                  overflow: TextOverflow.ellipsis,
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Theme.of(context).primaryColorDark,
-                    fontSize: 18,
+                Container(
+                  child: Image.asset(
+                    'assets/images/waiting.png',
+                    fit: BoxFit.cover,
                   ),
                 ),
-              ),
+              ],
+            )
+          : ListView.builder(
+              itemBuilder: (ctx, index) {
+                return Card(
+                  child: ListTile(
+                    title: Text(
+                      transactions[index].title,
+                      overflow: TextOverflow.ellipsis,
+                      // ignore: deprecated_member_use
+                      style: Theme.of(context).textTheme.title,
+                      // style: TextStyle(
+                      //   color: Colors.black87,
+                      // ),
+                    ),
+                    subtitle: Text(
+                      DateFormat('MMM dd, yyyy').format(transactions[index].date),
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    leading: Container(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 10,
+                      ),
+                      width: 120,
+                      child: Text(
+                        '\$${transactions[index].amount.toStringAsFixed(2)}',
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Theme.of(context).primaryColorDark,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              },
+              itemCount: transactions.length,
             ),
-          );
-        },
-        itemCount: transactions.length,
-      ),
     );
   }
 }
