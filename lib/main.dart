96d21bc59d556454d20c72a6f84e1f21d451f799
unknown <japerez.ph@gmail.com>
2020-08-10 20:10:21 +0800

    delete a transaction

--- lib/main.dart
+++ lib/main.dart
@@ -120,6 +120,12 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
+  void _deleteTransaction(String id) {
+    setState(() {
+      _transactions.removeWhere((transaction) => transaction.id == id);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -142,7 +148,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Chart(_recentTransactions),
-          TransactionList(_transactions),
+          TransactionList(_transactions, _deleteTransaction),
         ],
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -5,8 +5,9 @@ import '../models/transaction.dart';
 
 class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
+  final Function deleteTransaction;
 
-  TransactionList(this.transactions);
+  TransactionList(this.transactions, this.deleteTransaction);
 
   @override
   Widget build(BuildContext context) {
@@ -73,6 +74,13 @@ class TransactionList extends StatelessWidget {
                         fontSize: 12,
                         color: Colors.grey,
                       ),
+                    ),
+                    trailing: IconButton(
+                      icon: Icon(
+                        Icons.delete,
+                      ),
+                      color: Theme.of(context).errorColor,
+                      onPressed: () => deleteTransaction(transactions[index].id),
                     ),
                   ),
                 );
